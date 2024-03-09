9221e6566a97790498f0eebbc8d421817512b4af
Muhammedhanzala <156784275+Muhammedhanzala@users.noreply.github.com>
2024-03-09 13:40:29 +0500

    Add files via upload

---
+++ New folder (2)/app.js
@@ -0,0 +1,6 @@
+var num = prompt("please enter a number","");
+for(var i=1;i<=10;i++){
+    document.write("<div>");
+    document.write(num +" x " + i + "=" +  i*num);
+    document.write("</div>");
+}
